f8092f44c03e7dd8526c53c62b6748fbad412be6
Komalhimgori <komalhingori@gmail.com>
2024-10-29 10:32:49 -0700

    Add files via upload

---
+++ python/first.py
@@ -0,0 +1,37 @@
+#values of each no
+num1=10
+num2=5
+num3=15
+
+sum=num1+num2+num3
+difference=num1-num2-num3
+product=num1*num2*num3
+average=sum/3
+
+print(sum)
+print(difference)
+print(product)
+print(average)
+
+# Triangle
+base=5
+height=10
+triangle_area=0.5*base*height
+print("area of triangle",triangle_area)
+
+# Square
+side=4
+square_area=side**2
+print("area of square",square_area)
+
+# Circle
+import math
+radius=7
+circle_area=math.pi*radius**2
+print("area of circle",circle_area)
+
+# Rectangle
+length=8
+width=6
+rectangle_area=length*width
+print("area of rectangle",rectangle_area)
